6e05d16960cc608eb10f2667f5bfebfe7875e2aa
diegofred <diego2259@gmail.com>
2023-10-25 00:45:19 -0300

    from 16 to 19

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: %i[new create index]
+
   def index
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def show; end
 
   def new
     @product = Product.new
@@ -15,13 +15,32 @@ class ProductsController < ApplicationController
     @product = Product.new(secure_params)
 
     if @product.save
-      redirect_to products_path
+      redirect_to products_path, notice: 'Product was created successfully'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def update
+    if @product.update(secure_params)
+      redirect_to products_path, notice: 'The product was updated successfully'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: 'The product was deleted', status: :see_other
+  end
+
+  def edit; end
+
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def secure_params
     params.require(:product).permit(:title, :description, :price)
